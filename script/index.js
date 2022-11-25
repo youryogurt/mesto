ce4676663d057d36ab00a98c3984b872b73e7f31
Janna Gurtovaia <zhanna.gurt@gmail.com>
2022-11-25 23:07:37 +0400

    реализовать функционал на JS

--- script/index.js
+++ script/index.js
@@ -1,37 +1,34 @@
-const body = document.querySelector('body')
-
 const popupBackground = document.querySelector('.popup');
 const openPopupButton = document.querySelector('.popup-open');
 const closePopupButton = document.querySelector('.popup__close-button');
+const popup = document.querySelector('.popup__container');
 
+// открытие формы
 openPopupButton.addEventListener('click', function (event) {
-  event.preventDefault();
   popupBackground.classList.add('popup_opened')
 });
 
-closePopupButton.addEventListener('click', function () {
-  popupBackground.classList.remove('popup_opened');
-});
+// сабмит формы
+popup.addEventListener('submit', function (event) {
+  event.preventDefault();
 
-// popup.addEventListener('click', function(event) {
-//   if(event.target === event.currentTarget) {
-//     closePopup();
-//   }
-// });
-
-
-const saveButton = document.querySelector('.popup__save-button');
-let nameInput = document.querySelector('#name');
-let jobInput = document.querySelector('#job');
-
-saveButton.addEventListener('onsubmit', function (evt) {
-  
   const name = document.querySelector('.profile__full-name');
   const job = document.querySelector('.profile__job');
 
-  name.textContent = nameInput.value;
-  job.textContent = jobInput.value;
+  name.textContent = document.querySelector('#name').value;
+  job.textContent = document.querySelector('#job').value;
 
   popupBackground.classList.remove('popup_opened');
-  evt.preventDefault();
-}); 
+});
+
+// закрытие попапа по кнопке и по клику за пределами формы
+function closePopup () {
+  popupBackground.classList.remove('popup_opened');
+}
+
+closePopupButton.addEventListener('click', closePopup);
+popupBackground.addEventListener('click', closePopup);
+
+popup.addEventListener('click', function (event) {
+  event.stopPropagation();
+});
